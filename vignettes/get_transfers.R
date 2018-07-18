b2ecbdbca09075f148759514b1382ff1b0400a75
tbdv <tbuckley@bayareametro.gov>
2018-07-18 09:01:26 -0700

    add destination name

--- vignettes/get_transfers.R
+++ vignettes/get_transfers.R
@@ -5,10 +5,11 @@ library(dbplyr)
 library(dplyr)
 
 start <- as.Date("01-01-16",format="%m-%d-%y")
-end   <- as.Date("12-31-16",format="%m-%d-%y")
+end   <- as.Date("03-31-16",format="%m-%d-%y")
 
-dates <- sample(seq(start, end, by="day"), 40)
+dates <- sample(seq(start, end, by="day"),12)
 
 rs <- connect_rs()
 
-l_dfs <- lapply(dates, function(x) {sample_a_day(rs,x,users=100) %>% as_tibble()})
+l_dfs <- lapply(dates, function(x) {sample_a_day(rs,x,users=500) %>% as_tibble()})
+sample_df <- bind_rows(l_dfs)
