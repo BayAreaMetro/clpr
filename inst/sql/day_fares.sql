d3c88d33f4e41dc24f4765b8089379a1b2a7728d
tbdv <32847647+tbdv@users.noreply.github.com>
2018-08-16 11:07:10 -0700

    document sql script
    closes https://github.com/BayAreaMetro/clpr/issues/1

--- inst/sql/day_fares.sql
+++ inst/sql/day_fares.sql
@@ -1,3 +1,11 @@
+--=======================================================================================================
+--This query subset transactions to 1 travel model day. 
+--It takes parameters for start_day and end_day and the time at which the day should be partitioned
+--It also casts the UTC timestamp on the data to PST, using a function which accounts 
+--for daylight savings time
+--an example use of this query r is here:
+--https://github.com/BayAreaMetro/clpr/blob/d84eb0a39124ccdacd98badfb692894b4c4b98ae/R/extract.R#L84
+--=======================================================================================================
 create table ctp.fares_{date_title}
 SORTKEY(cardid_anony,
         generationtime)
